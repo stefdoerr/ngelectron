d84fc54c00d468b30f3155c1d937b070357e09e7
Stefan Doerr <stefan.doerr@upf.edu>
2018-04-09 23:44:04 +0200

    first working commit

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800,
+    });
+
+    mainWindow.loadUrl('file://' + __dirname + '/app/webapp.html');
+    mainWindow.setMenu(null);
+});
